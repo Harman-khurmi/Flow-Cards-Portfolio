c5cd5f91165c46a3c71bb233b4d15b70931a4c62
Harman-khurmi <hsingh2_be21@thapar.edu>
2024-09-07 13:44:40 +0530

    loadingAnimation

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,37 @@
-import React from 'react'
+import { React, useState, useEffect } from 'react'
 import Leftsection from './components/Leftsection'
 import Rightsection from './components/Rightsection'
+import PacmanLoader
+  from "react-spinners/PacmanLoader";
+import './App.css'
 
 
 function App() {
+  const [Loading, setLoading] = useState(false);
+  useEffect(() => {
+    setLoading(true)
+    setTimeout(() => {
+      setLoading(false)
+    }, 3000)
+  }, [])
   return (
     <>
-      <div className='bg-zinc-100/50 w-full min-h-screen tracking-tight flex flex-row overflow-x-hidden'>
-        <Leftsection />
-        <Rightsection />
+      <div className='bg-zinc-100/50 w-[100%] min-h-screen flex flex-row overflow-x-hidden relative'>
+        {Loading ?
+          <div className="loadingScreen">
+            <PacmanLoader
+              className="loaderAnimation"
+              color={"#08c6f5"}
+              loading={Loading}
+              size={50}
+            />
+            <h3 className='Instructions1'>You can drag the Cards.</h3>
+            <h3 className='Instructions2'>Double Click to Visit.</h3>
+          </div>
+
+          :
+          <><Leftsection /><Rightsection /></>
+        }
       </div>
     </>
   )
--- src/components/UserDetails.jsx
+++ src/components/UserDetails.jsx
@@ -6,24 +6,26 @@ function UserDetails() {
   };
   return (
     <>
-      <div className='h-[85vh] w-[36vw] flex-col  pl-16 pt-16 fixed' >
-        <img src={myImage} className='h-44 w-44 object-cover rounded-full' draggable="false" />
+      <div className='h-[85vh] w-[36vw] flex-col  pl-20 pt-16 fixed' >
+        <img src={myImage} className='h-[12rem] w-[12rem] object-cover rounded-full' draggable="false" />
         <div className='mt-8'>
-          <h1 className='font-semibold text-5xl'>Harmanpreet Singh</h1>
-          <p className='pr-5 pt-3 text-xl font-medium text-zinc-600 leading-6'>Exploring the techscape with a designer's eye and a coder's heart, I create magic in the pixels.
-            <br /><br />
-            ✦ Punjab, India
+          <h1 className='font-bold text-[44px]'>Harmanpreet Singh</h1>
+          <p className='pr-0 pt-3 text-xl font-light text-zinc-600 leading-[26px]'>Exploring the techscape with a designer's eye and a coder's heart, I create magic in the pixels.
             <br />
-            ✦ Web-Developer & UI/UX Designer</p>
+            <h3 className='py-1 pt-4'>✦ Sofware Engineer</h3>
+            <h3 className='py-1'>✦ Full Stack Web Developer</h3>
+            <h3 className='py-1'>✦ UI/UX Designer</h3>
+            <h3 className='py-1'>✦ Punjab, India</h3>
+          </p>
 
         </div>
         <div>
-        <button
-      onClick={handleRefresh}
-      className="bg-blue-500 text-white px-4 py-2 my-10 font-[400] rounded-md"
-    >
-      Reset Cards
-    </button>
+          <button
+            onClick={handleRefresh}
+            className="bg-blue-500 text-white px-4 py-2 my-10 font-[400] rounded-md"
+          >
+            Reset Cards
+          </button>
         </div>
       </div>
     </>
